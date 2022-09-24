0bf7abd00c63f973e498dc0e2eb5f272a1cf7467
zainuddin25 <zanukjepara@gmail.com>
2022-09-24 20:19:57 +0700

    About Modif

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from '../components/header/header'
 import Menubar from '../components/menubar/menubar'
 import About from './about'
+import Contact from './contact'
 import Hero from './hero'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
     </div>
     <Hero />
     <About />
+    <Contact />
     <div className='fixed bottom-0 w-full md:hidden lg:hidden xl:hidden 2xl:hidden'>
       <Menubar />
     </div>
