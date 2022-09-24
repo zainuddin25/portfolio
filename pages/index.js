5ec855a1a3d16d9e82a9fc770b29dbf623870b3e
zainuddin25 <zanukjepara@gmail.com>
2022-09-24 18:18:30 +0700

    Navbar Android

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Header from '../components/header'
 
 export default function Home() {
   return (
-    <div className="flex justify-center w-screen h-screen flex-col items-center">
-        <h1 className='text-2xl font-bold'>Ini Home</h1>
-        <div className="flex">
-            <Link href="/home/about">
-                <a className='px-4 py-2 border-sky-500 border rounded-sm mr-2'>About</a>
-            </Link>
-            <Link href="/home/contact">
-                <a className='px-4 py-2 border-sky-500 border rounded-sm ml-2'>Contact</a>
-            </Link>
-        </div>
-    </div>
+    <Header />
   )
 }
